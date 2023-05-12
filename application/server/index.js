0075a38abe9975d7927802eb2592cefa2292215f
Adithya Vasisth <adithyasvasisth@gmail.com>
2023-05-12 21:40:34 +0200

    check again

--- application/server/index.js
+++ application/server/index.js
@@ -23,11 +23,11 @@ app.get("/test", (req, res) => {
 // add the path of the audio file to the database
 app.post("/", upload.single("question"), (req, res) => {
   console.log("POST request received");
-  console.log(req.body);
-  console.log(req.file);
-  
-  const caller = req.body.caller;
-  // const infile = req.file.buffer;
+
+  // remove the + sign from the caller number and add timestamp
+  const timestamp = new Date().toISOString().slice(0, 19).replace("T", " ");
+  const caller = req.body.caller.replace("+", "") + "_" + timestamp;
+  const infile = req.file.buffer;
   const outfile_name = `audio-recording/${caller}.mp3`;
 
   // Upload the file to Google Cloud Storage
@@ -40,17 +40,17 @@ app.post("/", upload.single("question"), (req, res) => {
   console.log(`Caller: ${caller}`);
   console.log(`File name: ${outfile_name}`);
 
-  // blob.save(infile, (err) => {
-  //   if (err) {
-  //     console.error("Error uploading file:", err);
-  //     res.status(500).send("Error uploading file");
-  //   } else {
-  //     // Store the path URL in the DB for later retrieval
-  //     const pathUrl = blob.publicUrl();
-  //     add_entry(pathUrl);
-  //     res.status(200).send("File uploaded successfully");
-  //   }
-  // });
+  blob.save(infile, (err) => {
+    if (err) {
+      console.error("Error uploading file:", err);
+      res.status(500).send("Error uploading file");
+    } else {
+      // Store the path URL in the DB for later retrieval
+      const pathUrl = blob.publicUrl();
+      add_entry(pathUrl);
+      res.status(200).send("File uploaded successfully");
+    }
+  });
 });
 
 app.listen(port, () => {
